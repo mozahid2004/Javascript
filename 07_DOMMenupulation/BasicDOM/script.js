fff3f44bebb9572b2aac751f496428faf1ea1a83
Mozahid Izhar <mozahidizhar54@gmail.com>
2026-05-13 07:05:08 +0530

    Completed Basic DOM

---
+++ 07_DOMMenupulation/BasicDOM/script.js
@@ -0,0 +1,39 @@
+/**
+    There are 5 phases of dom manupulation
+      1.DOM
+      2.selection of HTML Element
+      3.How to chnage or manupulate HTML
+      4.How to chnage or manupulate CSS
+      5.Event Listener
+      
+ */
+
+// const p = document.querySelector("p");
+// const h1 = document.querySelector("h1");
+
+// p.innerHTML = "U r Hacked"
+// h1.innerHTML = "U r Hacked by MZ"
+// p.style.background = "red"
+
+// event Listners
+
+// let h1 = document.querySelector("h1");
+// const p = document.querySelector("p");
+
+// h1.addEventListener('click', () => {
+//   p.style.background = "rgba(0,255,0,0.6)"
+//   p.style.fontSize = "19px"
+//   h1.innerHTML = "Connected"
+// })
+
+
+// const p = document.querySelector("p");
+// const button = document.querySelector('button');
+
+// button.addEventListener('click',()=>{
+
+//   p.style.rotate = "360deg"
+
+// })
+
+// console.log(button.classList);
